a4590db2e4a631b0358a0ca00e37d73a113df821
julia Wager <julia@p-wagner.at>
2023-12-06 14:38:17 +0100

    fix: remove slash from allowed host heroku link

--- pipetracker/settings.py
+++ pipetracker/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['pipetracker.herokuapp.com/',
-                 'pipetracker-96d1f7c7a4dc.herokuapp.com/',
+ALLOWED_HOSTS = ['pipetracker.herokuapp.com',
+                 'pipetracker-96d1f7c7a4dc.herokuapp.com',
                  'localhost',
                  '127.0.0.1']
 
