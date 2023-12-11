cdb788ef8caa91089c240e217c69a393596d82ca
julia Wager <julia@p-wagner.at>
2023-12-11 20:22:18 +0100

    feat: install and set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#00B2CA",
+        dark: "#274060",
+        white: "#F9F9F9",
+        black: "#041020",
+      },
+    },
+  },
+  plugins: [],
+}
+
