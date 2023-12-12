7870ac8bbe7c16e730fde21624b3144348c8f6ab
julia Wager <julia@p-wagner.at>
2023-12-12 20:57:05 +0100

    feat: add header template

---
+++ static/js/base.js
@@ -0,0 +1,7 @@
+/*
+Toggle the mobile hamburger menu.
+*/
+document.getElementById("mobile-menu-toggle").addEventListener("click", function() {
+    console.log("test");
+    document.getElementById("navbar-default").classList.toggle('hidden');
+});
