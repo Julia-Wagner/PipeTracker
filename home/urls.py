6a34b6bdcd37a6a5c06ecbba49c1d79991f027dd
julia Wager <julia@p-wagner.at>
2023-12-13 20:36:26 +0100

    feat: add protected statistics view

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Index
-
+from django.contrib.auth.decorators import login_required
+from .views import Index, Statistics
 
 urlpatterns = [
-    path("", Index.as_view(), name="home")
+    path("", Index.as_view(), name="home"),
+    path("statistics/", login_required(Statistics.as_view()),
+         name="statistics"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "home/index.html"
+
+
+class Statistics(TemplateView):
+    template_name = "home/statistics.html"
