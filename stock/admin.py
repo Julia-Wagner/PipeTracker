990390ddec722dce32fb295935ca008ca7fea24d
julia Wager <julia@p-wagner.at>
2023-12-13 21:50:37 +0100

    feat: register category in admin

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "image", "image_alt", "parent", "order")
+    search_fields = ["name"]
+    prepopulated_fields = {"image_alt": ("name",)}
+    list_filter = ("parent",)
