9d45674a65332b17ec29cc526817cf7e44d52df9
julia Wager <julia@p-wagner.at>
2023-12-13 21:32:59 +0100

    feat: add category model

--- stock/models.py
+++ stock/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django_resized import ResizedImageField
+
+
+class Category(models.Model):
+    """
+    Model to manage categories
+    """
+    user = models.ForeignKey(User, related_name="created_categories",
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    image = ResizedImageField(
+        size=[500, None], quality=75, upload_to="categories/",
+        force_format="WEBP", null=False, blank=False
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    parent = models.ForeignKey("self", null=True, blank=True,
+                               related_name="children",
+                               on_delete=models.CASCADE)
+    order = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return str(self.name)
