4413703c0254d7d1f4211d33c761ff88c38bbbef
julia Wager <julia@p-wagner.at>
2023-12-14 20:52:16 +0100

    feat: add custom classes to form labels and inputs, not working yet

--- stock/forms.py
+++ stock/forms.py
@@ -1,11 +1,25 @@
 from django import forms
 from .models import Category
+from crispy_forms.helper import FormHelper
 
 
 class CategoryForm(forms.ModelForm):
     """
     Form to create a Category
     """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add custom classes to the form.
+        """
+        super(CategoryForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.label_class = "block mb-2 text-customblack"
+        self.helper.field_class = \
+            ("bg-gray-50 border border-darkblue text-gray-900 "
+             "text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 "
+             "block w-full p-2.5")
+
     class Meta:
         model = Category
         fields = ["name", "image", "image_alt", "parent", "order"]
