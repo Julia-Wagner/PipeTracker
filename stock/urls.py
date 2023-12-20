798e5b8443c6a49a0c307de0df70a19deaf5362a
julia Wager <julia@p-wagner.at>
2023-12-20 21:04:50 +0100

    fix: change url names to correctly show current nav page

--- stock/urls.py
+++ stock/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import (AddCategory, Categories, CategoriesChildren,
-                    DeleteCategory, EditCategory)
+                    DeleteCategory, EditCategory, Items)
 
 urlpatterns = [
     # categories
     path("", login_required(Categories.as_view()),
-         name="categories"),
+         name="stock_categories"),
     path("categories/<int:pk>/", login_required(CategoriesChildren.as_view()),
-         name="categories_children"),
+         name="stock_categories_children"),
     path("categories/add/", login_required(AddCategory.as_view()),
-         name="add_category"),
+         name="stock_add_category"),
     path("categories/edit/<int:pk>/", login_required(EditCategory.as_view()),
-         name="edit_category"),
+         name="stock_edit_category"),
     path("categories/delete/<int:pk>/",
-         login_required(DeleteCategory.as_view()), name="delete_category"),
+         login_required(DeleteCategory.as_view()),
+         name="stock_delete_category"),
+    # stock items
+    path("categories/<int:pk>/items/", login_required(Items.as_view()),
+         name="stock_items"),
 ]
