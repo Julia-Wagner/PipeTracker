6b7b91020cdd304b03329b6f088d6e6481bb7055
julia Wager <julia@p-wagner.at>
2023-12-20 21:44:29 +0100

    feat: add reusable template for add category/item button

--- static/js/base.js
+++ static/js/base.js
@@ -2,6 +2,13 @@
 Toggle the mobile hamburger menu.
 */
 document.getElementById("mobile-menu-toggle").addEventListener("click", function() {
-    console.log("test");
     document.getElementById("navbar-default").classList.toggle('hidden');
+});
+
+/*
+Toggle the dropdown button to add categories and items.
+*/
+document.getElementById("dropdown-button").addEventListener("click", function() {
+    console.log("click");
+    document.getElementById("dropdown-menu").classList.toggle('hidden');
 });
