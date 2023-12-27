24021793f3491a802db292d11c688f2bcc3ed51b
julia Wager <julia@p-wagner.at>
2023-12-27 20:39:17 +0100

    feat: register customer in admin

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("number", "first_name", "last_name")
+    search_fields = ["number", "first_name", "last_name"]
