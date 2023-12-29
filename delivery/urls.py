6cfb06491663284b81b9edce0b24149282700bbd
julia Wager <julia@p-wagner.at>
2023-12-29 21:40:45 +0100

    fix: change delivery urls to remove second /delivery/ in url

--- delivery/urls.py
+++ delivery/urls.py
@@ -5,8 +5,8 @@ from .views import (DeliveryNotes, AddNote, EditNote)
 urlpatterns = [
     path("", login_required(DeliveryNotes.as_view()),
          name="delivery_notes"),
-    path("delivery/add/", login_required(AddNote.as_view()),
+    path("add/", login_required(AddNote.as_view()),
          name="delivery_add_note"),
-    path("delivery/edit/<int:pk>/", login_required(EditNote.as_view()),
+    path("edit/<int:pk>/", login_required(EditNote.as_view()),
          name="delivery_edit_note"),
 ]
