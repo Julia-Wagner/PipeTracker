aa9f0843336b48d3fcc9377e553c9e14e27f13e3
julia Wager <julia@p-wagner.at>
2024-01-02 13:21:48 +0100

    feat: add basket and basketitem models

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from stock.models import Item
 
-# Create your models here.
+
+class Basket(models.Model):
+    """
+    Model to manage baskets
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item, through="BasketItem")
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"Basket for {self.user.username}"
+
+
+class BasketItem(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.item} ({self.quantity})"
