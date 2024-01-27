5c6c5710ce34c7fcb839ddb2f3c43d4b4ae0098b
julia Wager <julia@p-wagner.at>
2024-01-27 10:11:06 +0100

    feat: add robots.txt file

--- pipetracker/urls.py
+++ pipetracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,11 @@ urlpatterns = [
     path("stock/", include("stock.urls")),
     path("delivery/", include("delivery.urls")),
     path("basket/", include("basket.urls")),
+
+    # add robots.txt
+    # https://adamj.eu/tech/2020/02/10/robots-txt/
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain")),
 ]
 
 # custom error pages
